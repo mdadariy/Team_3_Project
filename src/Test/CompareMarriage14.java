6c012e6481dc4e1738d969a6093921da498245ca
ShreyanshVania <shreyanshvania@gmail.com>
2015-10-01 15:16:34 -0400

    User story 12(CompareMarriage14)

---
+++ src/Test/CompareMarriage14.java
@@ -0,0 +1,27 @@
+package Test;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class CompareMarriage14 {
+
+    public static void compare(String birthDateString, String marriageDateString) {
+
+        try {
+            SimpleDateFormat format = new SimpleDateFormat("dd MMM yyyy");
+
+            Date birthDate = format.parse(birthDateString);
+            Date marriageDate = format.parse(marriageDateString);
+
+            long diff = marriageDate.getTime() - birthDate.getTime();
+            long diffHours = diff / (60 * 60 * 1000);
+            long diffDays = diffHours / 24; // 48
+            long diffYear = diffDays / 365;
+            if (diffYear < 14) {
+                System.out.println("Error: Marriage before 14 years old.");
+            }
+        } catch (Exception ex) {
+
+        }
+    }
+}
