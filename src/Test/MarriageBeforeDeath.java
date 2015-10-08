f070d832a53f6a7adcefbfdcc202cfda756f2765
mukta <mdadariy@stevens.edu>
2015-10-08 12:48:45 -0400

    User Story 7 : Marriage Before Death

---
+++ src/Test/MarriageBeforeDeath.java
@@ -0,0 +1,26 @@
+package src.Test;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by Mukta on 10/8/15.
+ * User Story 7
+ */
+public class MarriageBeforeDeath{
+    public static boolean compare (String marriageDateString, String deathDateString) throws ParseException {
+        SimpleDateFormat format = new SimpleDateFormat("dd MMM yyyy");
+
+        Date marriageDate = format.parse(marriageDateString);
+        Date divorceDate = format.parse(deathDateString);
+
+        if (marriageDate.compareTo(divorceDate) == 1) {
+            System.out.println("Error: Marriage date is after the Death date.");
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+}
