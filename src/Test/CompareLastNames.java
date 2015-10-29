68b1818905c100a2888018a0ad49754be460c586
ShreyanshVania <shreyanshvania@gmail.com>
2015-10-29 17:18:46 -0400

    user story 13(CompareLastNames)

---
+++ src/Test/CompareLastNames.java
@@ -0,0 +1,19 @@
+package Test;
+
+import java.util.List;
+
+/**
+ * Created by Shreyansh on 08-10-2015.
+ */
+public class CompareLastNames {
+    public static boolean check(List<String> nameStringList) {
+
+        for (int i = 0; i < nameStringList.size() - 1; i++) {
+            for (int j = i + 1; j < nameStringList.size(); j++) {
+                if (!nameStringList.get(i).equals(nameStringList.get(j)))
+                    return false;
+            }
+        }
+        return true;
+    }
+}
