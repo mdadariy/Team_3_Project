22b7dcecb3926d7e5a8d193afcb50f1ceb854f5b
dinever <dinever@live.cn>
2015-11-09 14:40:27 -0500

    Refactoring

--- src/Test/ListLargeAgeDifference.java
+++ src/Test/ListLargeAgeDifference.java
@@ -1,19 +1,21 @@
-package test;
+package Test;
 
 /**
  * Created by Shreyansh on 08-10-2015.
  */
-public class listlargeagedifference {
+import Model.Family;
 
-// User Story 17
+public class ListLargeAgeDifference {
+    // User Story 17
+    public static void check(Family fam) {
+        if ((fam.getHusband().getAge() >= 2*fam.getWife().getAge())
+                || (2*fam.getHusband().getAge() <= fam.getWife().getAge()))
+        {
+//            System.out.println(fam.getHusband().getAge());
+//            System.out.println(fam.getWife().getAge());
 
-    if ((fam.getHusband().getAge() >= 2*fam.getWife().getAge())
-            || (2*fam.getHusband().getAge() <= fam.getWife().getAge()))
-    {
-        System.out.println(fam.getHusband().getAge());
-        System.out.println(fam.getWife().getAge());
-
-        System.out.println("Husband and Wife has large age difference. \nHusband: " + fam.getHusband().getName() +
-                "\t Wife: " + fam.getWife().getName());
+            System.out.println("Error: Husband and Wife has large age difference. \nHusband: " + fam.getHusband().getName() +
+                    "\t Wife: " + fam.getWife().getName());
+        }
     }
 }
